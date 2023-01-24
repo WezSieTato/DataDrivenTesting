20592307a79e69fc95b7077e59fe479b88e01eb7
Marcin Stepnowski <marcin.stepnowski@gmail.com>
2023-01-24 12:57:49 +0100

    chore: add swiftlint

--- Sources/DataDrivenTesting/TestData.swift
+++ Sources/DataDrivenTesting/TestData.swift
@@ -1,18 +1,17 @@
 import Foundation
 
 public struct TestData<DataType> {
-  public var data: DataType
-  public var file: StaticString
-  public var line: UInt
-  
-  public init(
-    _ data: DataType,
-    file: StaticString = #file,
-    line: UInt = #line)
-  {
-    self.data = data
-    self.file = file
-    self.line = line
-  }
-  
+    public var data: DataType
+    public var file: StaticString
+    public var line: UInt
+
+    public init(
+        _ data: DataType,
+        file: StaticString = #file,
+        line: UInt = #line
+    ) {
+        self.data = data
+        self.file = file
+        self.line = line
+    }
 }
