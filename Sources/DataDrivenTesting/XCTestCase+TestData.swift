5b2ec2e5647d9076143e4f5e690179419918a8a6
Marcin Stepnowski <marcin.stepnowski@gmail.com>
2023-01-25 11:28:50 +0100

    chore: add swiftlint (#3)
    * chore: add swiftlint
    
    * chore: format files
    
    * fix mintfile

--- Sources/DataDrivenTesting/XCTestCase+TestData.swift
+++ Sources/DataDrivenTesting/XCTestCase+TestData.swift
@@ -1,17 +1,15 @@
 import XCTest
 
 public extension XCTestCase {
-  func dataTests<DataType>(
-    _ dataArray: [TestData<DataType>],
-    _ testClosure: (TestData<DataType>, XCTActivity) -> ())
-  {
-      dataArray.forEach { testData in
-        let name = "When data is \(testData.data)"
-        XCTContext.runActivity(named: name) { activity in
-            testClosure(testData, activity)
+    func dataTests<DataType>(
+        _ dataArray: [TestData<DataType>],
+        _ testClosure: (TestData<DataType>, XCTActivity) -> Void
+    ) {
+        dataArray.forEach { testData in
+            let name = "When data is \(testData.data)"
+            XCTContext.runActivity(named: name) { activity in
+                testClosure(testData, activity)
+            }
         }
-        
     }
-  }
 }
-
--- Tests/DataDrivenTestingTests/DataDrivenTests.swift
+++ Tests/DataDrivenTestingTests/DataDrivenTests.swift
@@ -1,16 +1,15 @@
-import XCTest
 @testable import DataDrivenTesting
+import XCTest
 
 final class DataDrivenTests: XCTestCase {
     func testDescriptionFromInteger() {
         dataTests([
             TestData((input: 1, expected: "1")),
-            TestData((input: 2, expected: "2"))
-        ]) { testData, activity in
+            TestData((input: 2, expected: "2")),
+        ]) { testData, _ in
             let result = testData.data.input.description
-            
+
             XCTAssertEqual(result, testData.data.expected, file: testData.file, line: testData.line)
         }
-
     }
 }
